916af86fd1873bec9853155f4ce1111a811ecba5
Ben <30612894+BenHavis@users.noreply.github.com>
2023-07-01 18:51:26 -0400

    Add files via upload

--- src/utils/geocodeCity.js
+++ src/utils/geocodeCity.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+const axios = require('axios');
 
-export const geocodeCity = async (city, state, country) => {
+exports.geocodeCity = async (city, state, country) => {
   try {
     const address = `${city}, ${state}, ${country}`;
     const encodedAddress = encodeURIComponent(address);
-		const apiKey = 'AIzaSyBsY7TWsifdD_oYfwhEKBqVdLzfBaSeu6A'
+    const apiKey = 'AIzaSyBsY7TWsifdD_oYfwhEKBqVdLzfBaSeu6A'
 
     const response = await axios.get(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${apiKey}`
