afc53627d9253868943667eb9683de664417b112
Ben <30612894+BenHavis@users.noreply.github.com>
2023-07-07 15:14:37 -0400

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
-// import { geocodeCity } from './utils/geocodeCity';
-const { geocodeCity } = require('./utils/geocodeCity')
+const axios = require('axios');
 const express = require('express')
 const { Sequelize, DataTypes } = require('sequelize')
 const { GOOGLE_MAPS_API_KEY } = require('./config.js')
@@ -9,6 +8,34 @@ const jwt = require('jsonwebtoken')
 
 // Create an instance of Express
 const app = express()
+
+const geocodeCity = async (city, state, country) => {
+  try {
+    const address = `${city}, ${state}, ${country}`;
+    const encodedAddress = encodeURIComponent(address);
+		const apiKey = 'AIzaSyBsY7TWsifdD_oYfwhEKBqVdLzfBaSeu6A'
+
+    const response = await axios.get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${apiKey}`
+    );
+
+    const results = response.data.results;
+    console.log('geocode results:', results);
+
+    if (results && results.length > 0) {
+      const result = results[0];
+      const { lat, lng } = result.geometry.location;
+      return { latitude: lat, longitude: lng };
+    } else {
+      console.log('No results found for the city:', city, state, country);
+      return null;
+    }
+  } catch (error) {
+    console.log('Error geocoding:', error);
+    return null;
+  }
+};
+
 
 // Configure the PostgreSQL connection
 const sequelize = new Sequelize('postgres://postgres:julius23!@localhost:5432/postgres')
